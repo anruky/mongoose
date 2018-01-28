913d30e1223daa23731497f4caf10432247fdc7d
anruky <anruky@gmail.com>
2018-01-28 15:24:01 +0800

    commit 1

---
+++ index.js
@@ -0,0 +1,36 @@
+mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+
+    console.log('Connected correctly to server');
+    
+    var newDish = Dishes({
+        name: 'Uthappizza5',
+        description: 'test'
+    });
+  
+    newDish.save()
+        .then((dish) => {
+            console.log(dish);
+
+            return Dishes.find({}).exec();
+        })
+        .then((dishes) => {
+            console.log(dishes);
+
+            return db.connection.db.dropCollection("dishes");
+        })
+        .then(() => {
+            return db.disconnect();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+});
